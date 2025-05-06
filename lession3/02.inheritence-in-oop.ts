30e69dba1fe013a7f9dab3b62f632155c6f719b5
Hera <mostafizhera@gmail.com>
2025-05-07 00:08:52 +0600

    inheritence in typescritp

---
+++ lession3/02.inheritence-in-oop.ts
@@ -0,0 +1,63 @@
+{
+
+    // createing a parent class 
+
+    class Parent {  /// create a parent class and reuse child classes.. 
+        name: string;
+        age: number;
+        address: string;
+
+
+        constructor(name: string, age: number, address: string) { // create constructor 
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        getSleep(numberOfHourse: number) { /// create a function and pass the paramiter
+            console.log(`${this.name} will sleep for ${numberOfHourse}`);
+
+        }
+    }
+
+
+    /// create an child class 
+
+    class Student extends Parent{  /// create class || extends use kora parent class er propety gula child class a use kortesi 
+
+        constructor(name: string, age: number, address: string) { // create constructor 
+            super(name, age, address)
+        }
+    }
+
+    const student1 = new Student("Mr. student", 20, "Uganda")  /// calling the student class and pass the arguments
+    console.log(student1.name);
+
+
+
+    // create teacher class --->>
+
+    class Teacher extends Parent{
+        designation: string; // because designation not assigned Parent class 
+
+        constructor(name: string, age: number, address: string, designation: string) {
+            super(name, age, address)
+            this.designation = designation;
+        }
+
+        takeClass(numOfHourse: number) {
+            console.log(`${this.name} will sleep for ${numOfHourse}`);
+
+        }
+    }
+
+    const teacherResults = new Teacher("Mostafiz", 22, "Bangladesg", "developer")
+    console.log(teacherResults);
+    
+
+}
+
+
+/// Notic....
+
+// inheritence holo emon ekta class ja reuse kora jai child class a use kora jai.
