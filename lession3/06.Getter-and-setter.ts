63a7f35c29d51bac6f5eaa334cc569be856eba0c
Hera <mostafizhera@gmail.com>
2025-05-08 00:22:30 +0600

    getter and setter useing on typescirpt

---
+++ lession3/06.Getter-and-setter.ts
@@ -0,0 +1,57 @@
+{
+
+    /// learn Getter and setter on typescript
+
+
+    class BankAccount {
+        public id: number;   // this is public property and access the global scope 
+        public name: string; // this is public property and access the global scope 
+        protected _balance: number /// only access localscope || [_] this is anventionaly/private property
+
+
+        constructor(id: number, name: string, _balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = _balance;
+        }
+
+        /// Useing setter 
+
+        set deposite(depositeAmount: number) {
+           this._balance = this._balance + depositeAmount;
+        }
+
+        // addDeposit(depositAmount: number){
+        //     this._balance = this._balance + depositAmount;
+        // }
+
+        /// Useing getter 
+
+        get balance() {
+            return this._balance;
+        }
+
+        // getBalance(){
+        //     return this._balance;
+        // }
+    }
+
+    class studentAccount extends BankAccount {
+        test() {
+            return this._balance
+        }
+    }
+
+    const GoribAccount = new BankAccount(23, "Dutch Bangla Bank", 60)
+    // GoribAccount.addDeposit(70)
+    // console.log(GoribAccount.getBalance());
+    GoribAccount.deposite = 70 // useing setter // create a function but useing property || There is very simple
+    console.log(GoribAccount.balance);  /// getter use kore amara function k property akyare use korte pertesi...
+
+
+
+}
+
+/// Notic ..
+
+// jodi emon kono kaj hoy jokhon amader parent class er kono kicu child class a use korle but secure vabe tokhon amara ==>>  protected  <<== use korbo.
